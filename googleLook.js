9f90c51f60c3665b470b2d34f0b3260c28da6bed
Hip Chip <hchip6207@gmail.com>
2026-03-18 14:32:04 -0700

    Delete googleLook.js

--- googleLook.js
+++
@@ -1,9 +0,0 @@
-const icon = document.createElement("link");
-icon.rel = "icon";
-icon.type = "image/x-icon";
-icon.src = ""; //Add favicon's source in the string
-const description = document.createElement("meta");
-description.name = "description";
-description.content = ""; //Put description in the string
-document.head.appendChild(icon);
-document.head.appendChild(description);
