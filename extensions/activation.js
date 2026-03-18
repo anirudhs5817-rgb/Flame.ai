699ad3302f778f094efacb77dd228d6b28bc59af
Hip Chip <hchip6207@gmail.com>
2026-03-18 15:01:07 -0700

    Create activation.js

---
+++ extensions/activation.js
@@ -0,0 +1,18 @@
+const extension1 = JSON.parse(localStorage.getItem("extension1"));
+const extension2 = JSON.parse(localStorage.getItem("extension2"));
+const extension3 = JSON.parse(localStorage.getItem("extension3"));
+const extensionLimitError = document.createElement("div");
+extensionLimitError.style.position = "fixed";
+extensionLimitError.style.bottom = "0px";
+extensionLimitError.style.width = "100%";
+extensionLimitError.style.height = "16%";
+extensionLimitError.style.zIndex = 999;
+extensionLimitError.innerHTML = "Exceeded extension limit";
+extensionLimitError.style.fontWeight = "bold";
+extensionLimitError.style.fontFamily = "Courier New";
+if (localStorage.getItem("extension4") === null) {} else {
+  document.body.appendChild(extensionLimitError);
+  setTimeout(function() {
+    extensionLimit.remove();
+  }, 3000);
+}
